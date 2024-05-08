fa1075e6c85ca7416c7a5c571ecd3ffcc6570e86
jelainemartins <jcm210891@gmail.com>
2024-05-08 20:23:42 -0300

    Criando um sistema e abstraindo um celular com poo.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
-
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+var nokia = new Nokia("1254", "nokia", "1208", 200);
+var iphone = new Iphone("1111", "hipone", "20", 100);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Maia");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Candy Crush");
